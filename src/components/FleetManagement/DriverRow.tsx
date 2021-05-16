35f830bd24b90d0b8e486b5873232a22ef7b8246
Mike Babetchki <mike.babetchki@gmail.com>
2021-05-16 04:59:25 -0500

    complete delete mutation and add dialog pop-up for confirmation

--- src/components/FleetManagement/DriverRow.tsx
+++ src/components/FleetManagement/DriverRow.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { useState } from "react"
 import {
   withStyles,
   Theme,
@@ -15,6 +16,12 @@ import {
   InputBase,
   FormControl,
   Checkbox,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Button,
 } from "@material-ui/core"
 import { CheckBox, CheckBoxOutlineBlank } from "@material-ui/icons"
 import EditButton from "../../components/EditButton"
@@ -59,204 +66,273 @@ const UPDATE_DRIVER = gql`
     }
   }
 `
+const DELETE_DRIVER = gql`
+  mutation Delete($id: uuid!) {
+    delete_fleet_table_by_pk(id: $id) {
+      driver
+    }
+  }
+`
 
 const DriverRow = props => {
   const [updateDriver] = useMutation(UPDATE_DRIVER)
+  const [deleteDriver] = useMutation(DELETE_DRIVER)
   const { id, driver, cell, truck, trailer, type, team, hazmat, tanker } = props
   const classes = useStyles()
 
+  const [open, setOpen] = useState(false)
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
   return (
-    <Formik
-      initialValues={{
-        id: id,
-        driver: driver,
-        cell: cell,
-        truck: truck,
-        trailer: trailer,
-        type: type,
-        team: team,
-        hazmat: hazmat,
-        tanker: tanker,
-        edit: false,
-      }}
-      onSubmit={values => {
-        updateDriver({
-          variables: {
-            id: values.id,
-            driver: values.driver,
-            cell: values.cell,
-            truck: parseInt(values.truck),
-            trailer: parseInt(values.trailer),
-            type: values.type,
-            team: values.team,
-            hazmat: values.hazmat,
-            tanker: values.tanker,
-          },
-        })
-      }}
-    >
-      {({ values, setFieldValue, handleSubmit }) => {
-        console.log(values)
-        return (
-          <StyledTableRow key={id}>
-            {/* Enables Editing Mode for each row */}
+    <>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Are you sure you want to delete this driver?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            This will permanently delete the driver entry!
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Go Back
+          </Button>
+          <Button
+            onClick={() =>
+              deleteDriver({
+                variables: {
+                  id: id,
+                },
+              })
+            }
+            color="primary"
+            autoFocus
+          >
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Formik
+        initialValues={{
+          id: id,
+          driver: driver,
+          cell: cell,
+          truck: truck,
+          trailer: trailer,
+          type: type,
+          team: team,
+          hazmat: hazmat,
+          tanker: tanker,
+          edit: false,
+        }}
+        onSubmit={values => {
+          updateDriver({
+            variables: {
+              id: values.id,
+              driver: values.driver,
+              cell: values.cell,
+              truck: parseInt(values.truck),
+              trailer: parseInt(values.trailer),
+              type: values.type,
+              team: values.team,
+              hazmat: values.hazmat,
+              tanker: values.tanker,
+            },
+          })
+        }}
+      >
+        {({ values, setFieldValue, handleSubmit }) => {
+          return (
+            <StyledTableRow key={id}>
+              {/* Enables Editing Mode for each row */}
 
-            <StyledTableCell>
-              {values.edit === false ? (
-                <EditButton
-                  size="small"
-                  color="primary"
-                  variant="outlined"
-                  click={() => {
-                    setFieldValue("edit", true, false)
-                  }}
-                />
-              ) : (
-                <UpdateButton
-                  size="small"
-                  color="secondary"
-                  variant="contained"
-                  click={() => {
-                    handleSubmit()
-                    setFieldValue("edit", false, false)
-                  }}
-                />
-              )}
-            </StyledTableCell>
-
-            <StyledTableCell>
-              {values.edit === false ? (
-                <Typography>{values.driver}</Typography>
-              ) : (
-                <Field
-                  name="driver"
-                  type="input"
-                  size="small"
-                  color="secondary"
-                  variant="outlined"
-                  as={CustomField}
-                />
-              )}
-            </StyledTableCell>
-            <StyledTableCell>
-              {values.edit === false ? (
-                <Typography>{values.cell}</Typography>
-              ) : (
-                <Field
-                  name="cell"
-                  type="input"
-                  size="small"
-                  color="secondary"
-                  variant="outlined"
-                  as={CustomField}
-                />
-              )}
-            </StyledTableCell>
-            <StyledTableCell>
-              {values.edit === false ? (
-                <Typography>{values.truck}</Typography>
-              ) : (
-                <Field
-                  name="truck"
-                  type="input"
-                  size="small"
-                  color="secondary"
-                  variant="outlined"
-                  as={CustomField}
-                />
-              )}
-            </StyledTableCell>
-            <StyledTableCell>
-              {values.edit === false ? (
-                <Typography>{values.trailer}</Typography>
-              ) : (
-                <Field
-                  name="trailer"
-                  type="input"
-                  size="small"
-                  color="secondary"
-                  variant="outlined"
-                  as={CustomField}
-                />
-              )}
-            </StyledTableCell>
-
-            <StyledTableCell>
-              {values.edit === false ? (
-                <Typography>{values.type}</Typography>
-              ) : (
-                <FormControl variant="outlined" className={classes.formControl}>
-                  <Field
-                    as={Select}
-                    name="type"
+              <StyledTableCell>
+                {values.edit === false ? (
+                  <EditButton
+                    size="small"
+                    color="primary"
                     variant="outlined"
-                    input={<CustomInput />}
-                  >
-                    <MenuItem value="53' Van">53' Van</MenuItem>
-                    <MenuItem value="53' Reefer">53' Reefer</MenuItem>
-                  </Field>
-                </FormControl>
-              )}
-            </StyledTableCell>
-            <StyledTableCell>
-              {values.edit === false ? (
-                <Typography>{values.team}</Typography>
-              ) : (
-                <FormControl variant="outlined" className={classes.formControl}>
+                    click={() => {
+                      setFieldValue("edit", true, false)
+                    }}
+                  />
+                ) : (
+                  <UpdateButton
+                    size="small"
+                    color="secondary"
+                    variant="contained"
+                    click={() => {
+                      handleSubmit()
+                      setFieldValue("edit", false, false)
+                    }}
+                  />
+                )}
+              </StyledTableCell>
+
+              <StyledTableCell>
+                {values.edit === false ? (
+                  <Typography>{values.driver}</Typography>
+                ) : (
                   <Field
-                    as={Select}
-                    name="team"
+                    name="driver"
+                    type="input"
+                    size="small"
+                    color="secondary"
                     variant="outlined"
-                    input={<CustomInput />}
+                    as={CustomField}
+                  />
+                )}
+              </StyledTableCell>
+              <StyledTableCell>
+                {values.edit === false ? (
+                  <Typography>{values.cell}</Typography>
+                ) : (
+                  <Field
+                    name="cell"
+                    type="input"
+                    size="small"
+                    color="secondary"
+                    variant="outlined"
+                    as={CustomField}
+                  />
+                )}
+              </StyledTableCell>
+              <StyledTableCell>
+                {values.edit === false ? (
+                  <Typography>{values.truck}</Typography>
+                ) : (
+                  <Field
+                    name="truck"
+                    type="input"
+                    size="small"
+                    color="secondary"
+                    variant="outlined"
+                    as={CustomField}
+                  />
+                )}
+              </StyledTableCell>
+              <StyledTableCell>
+                {values.edit === false ? (
+                  <Typography>{values.trailer}</Typography>
+                ) : (
+                  <Field
+                    name="trailer"
+                    type="input"
+                    size="small"
+                    color="secondary"
+                    variant="outlined"
+                    as={CustomField}
+                  />
+                )}
+              </StyledTableCell>
+
+              <StyledTableCell>
+                {values.edit === false ? (
+                  <Typography>{values.type}</Typography>
+                ) : (
+                  <FormControl
+                    variant="outlined"
+                    className={classes.formControl}
                   >
-                    <MenuItem value="Alex">Alex</MenuItem>
-                    <MenuItem value="Mike">Mike</MenuItem>
-                    <MenuItem value="Chip">Chip</MenuItem>
-                  </Field>
-                </FormControl>
-              )}
-            </StyledTableCell>
-            <StyledTableCell>
-              {values.edit === false ? (
-                values.hazmat === true ? (
-                  <CheckBox color="secondary" />
+                    <Field
+                      as={Select}
+                      name="type"
+                      variant="outlined"
+                      input={<CustomInput />}
+                    >
+                      <MenuItem value="53' Van">53' Van</MenuItem>
+                      <MenuItem value="53' Reefer">53' Reefer</MenuItem>
+                    </Field>
+                  </FormControl>
+                )}
+              </StyledTableCell>
+              <StyledTableCell>
+                {values.edit === false ? (
+                  <Typography>{values.team}</Typography>
                 ) : (
-                  <CheckBoxOutlineBlank color="secondary" />
-                )
-              ) : (
-                <Field
-                  name="hazmat"
-                  as={Checkbox}
-                  id={id}
-                  checked={values.hazmat}
-                />
-              )}
-            </StyledTableCell>
-            <StyledTableCell>
-              {values.edit === false ? (
-                values.tanker === true ? (
-                  <CheckBox color="secondary" />
+                  <FormControl
+                    variant="outlined"
+                    className={classes.formControl}
+                  >
+                    <Field
+                      as={Select}
+                      name="team"
+                      variant="outlined"
+                      input={<CustomInput />}
+                    >
+                      <MenuItem value="Alex">Alex</MenuItem>
+                      <MenuItem value="Mike">Mike</MenuItem>
+                      <MenuItem value="Chip">Chip</MenuItem>
+                    </Field>
+                  </FormControl>
+                )}
+              </StyledTableCell>
+              <StyledTableCell>
+                {values.edit === false ? (
+                  values.hazmat === true ? (
+                    <CheckBox color="secondary" />
+                  ) : (
+                    <CheckBoxOutlineBlank color="secondary" />
+                  )
                 ) : (
-                  <CheckBoxOutlineBlank color="secondary" />
-                )
-              ) : (
-                <Field
-                  name="tanker"
-                  as={Checkbox}
-                  id={id}
-                  checked={values.tanker}
-                />
-              )}
-            </StyledTableCell>
-            <StyledTableCell>
-              {values.edit === false ? (
-                <DeleteButton variant="outlined" size="small" click="" />
-              ) : null}
-            </StyledTableCell>
-          </StyledTableRow>
-        )
-      }}
-    </Formik>
+                  <Field
+                    name="hazmat"
+                    as={Checkbox}
+                    id={id}
+                    checked={values.hazmat}
+                  />
+                )}
+              </StyledTableCell>
+              <StyledTableCell>
+                {values.edit === false ? (
+                  values.tanker === true ? (
+                    <CheckBox color="secondary" />
+                  ) : (
+                    <CheckBoxOutlineBlank color="secondary" />
+                  )
+                ) : (
+                  <Field
+                    name="tanker"
+                    as={Checkbox}
+                    id={id}
+                    checked={values.tanker}
+                  />
+                )}
+              </StyledTableCell>
+              <StyledTableCell>
+                {values.edit === false ? (
+                  <DeleteButton
+                    variant="outlined"
+                    size="small"
+                    click={() => setOpen(true)}
+                    // click={values => {
+                    //   deleteDriver({
+                    //     variables: {
+                    //       id: id,
+                    //     },
+                    //   })
+                    // }}
+                  />
+                ) : null}
+              </StyledTableCell>
+            </StyledTableRow>
+          )
+        }}
+      </Formik>
+    </>
   )
 }
 
